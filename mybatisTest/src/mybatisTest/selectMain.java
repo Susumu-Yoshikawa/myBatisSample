9422f56f254dd8f037443704f4da94782e1df205
Susumu-Yoshikawa <susumu_yoshikawa@tbn.co.jp>
2014-11-19 21:53:53 +0900

    test

---
+++ mybatisTest/src/mybatisTest/selectMain.java
@@ -0,0 +1,30 @@
+package mybatisTest;
+
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class selectMain {
+
+	public static void main(String[] args) throws Exception {
+		Reader reader = Resources.getResourceAsReader("mybatisTest/mybatis-config.xml");
+        SqlSession session = new SqlSessionFactoryBuilder().build(reader).openSession();
+        Automobile sample1 = (Automobile)session.selectOne("select", 1);
+        System.out.println(sample1);
+        // List
+        List<Automobile> sample2 = (List<Automobile>)session.selectList("select_all");
+        for (Automobile a : sample2) {
+            System.out.println("a = [" + a + "]");
+        }
+        // Insert
+        Automobile insert_data = new Automobile(11,"bbb" , "foo" , 19790114);
+        session.insert("insert", insert_data);
+
+        session.commit();
+
+	}
+
+}
